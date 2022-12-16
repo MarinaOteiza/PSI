864538be0c5c15e8f172b7ba1f26f5b94511d756
Àitor Olivares Perucho <aitor.olivares@estudiants.urv.cat>
2022-12-16 10:22:03 +0100

    Peticiones hechos

---
+++ src/main/java/Productes/LlistaServei.java
@@ -0,0 +1,20 @@
+package Productes;
+
+public class LlistaServei extends Peticiones {
+    private Servei LlistaS[];
+    private int numllist;
+
+    public LlistaServei(String codi, String usuari, String producte, String tipus_producte, int num){
+        super(codi,usuari,producte,tipus_producte);
+        LlistaS= new Servei[num];
+        numllist=0;
+    }
+
+    public void addS(Servei s1){
+        if(numllist<=LlistaS.length){
+            LlistaS[numllist]=s1;
+            numllist++;
+        }
+    }
+
+}
---
+++ src/main/java/Productes/Servei.java
@@ -0,0 +1,28 @@
+package Productes;
+
+public class Servei {
+    private String peticio_interc;
+    private String usuari_petic;
+    private int valoracio_usuari;
+    private int valoracio_usupetic;
+    private boolean peticio_AoD;            //Peticio acceptada o denegada
+
+    public Servei(String petiint, String usupeti){
+        peticio_interc=petiint;
+        usuari_petic=usupeti;
+        valoracio_usuari=0;
+        valoracio_usupetic=0;
+        peticio_AoD=false;
+    }
+
+    public void contestarS(Servei s1, boolean AoD){
+        if(AoD==true){
+         s1.peticio_AoD=true;
+        }
+    }
+
+    public void valoracioS(int valUsu, int valClient){
+        valoracio_usuari=valUsu;
+        valoracio_usupetic=valClient;
+    }
+}
